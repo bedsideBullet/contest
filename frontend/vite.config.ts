6bbf53be3198929297b89955714ecde1c9f62676
Daniel Smith <danjsmith3@gmail.com>
2025-02-24 11:35:14 -0600

    vit-config

--- frontend/vite.config.ts
+++ frontend/vite.config.ts
@@ -1,7 +1,16 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react";
 
-// https://vite.dev/config/
+// https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react()],
-})
+	plugins: [react()],
+	base: "/", // Ensure this is correctly set for your deployment
+	build: {
+		outDir: "dist",
+	},
+	server: {
+		proxy: {
+			"/api": "https://contest-7b07.onrender.com",
+		},
+	},
+});
