eb3a5cc09eb8daf79e0a270b6563e2c30e4eaf86
Daniel Smith <danjsmith3@gmail.com>
2025-02-25 09:38:28 -0600

    agg

--- backend/src/app.ts
+++ backend/src/app.ts
@@ -1,32 +1,3 @@
-// import express from "express";
-// import cors from "cors";
-// import bodyParser from "body-parser";
-// import routes from "./routes";
-// import dotenv from "dotenv";
-
-// dotenv.config();
-
-// const app = express();
-// const port = process.env.PORT;
-
-// app.use(
-// 	cors({
-// 		origin: process.env.FRONTEND_URL,
-// 		credentials: true,
-// 		optionsSuccessStatus: 200,
-// 	})
-// );
-
-// app.options("*", cors());
-
-// app.use(express.json());
-
-// app.use("/api", routes);
-
-// app.listen(port,  () => {
-// 	console.log(`Server is running on port ${port}`);
-// });
-
 import express from "express";
 import cors from "cors";
 import bodyParser from "body-parser";
@@ -36,7 +7,7 @@ import dotenv from "dotenv";
 dotenv.config();
 
 const app = express();
-const port = parseInt(process.env.PORT || "10000", 10);
+const port = process.env.PORT;
 
 app.use(
 	cors({
@@ -52,6 +23,6 @@ app.use(express.json());
 
 app.use("/api", routes);
 
-app.listen(port, "0.0.0.0", () => {
+app.listen(port, () => {
 	console.log(`Server is running on port ${port}`);
 });
